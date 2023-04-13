524a1bc2ad21d29b80b6ba863c96c2e7a85f9946
DrMaxNix <contact@drmaxnix.de>
2023-04-13 14:14:48 +0200

    :tada: basic spigot plugin structure

---
+++ src/de/drmaxnix/featherhome/Main.java
@@ -0,0 +1,15 @@
+package de.drmaxnix.featherhome;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	@Override
+	public void onEnable(){
+		/**/System.out.println("[DEBUG] onEnable");
+	}
+	
+	@Override
+	public void onDisable(){
+		/**/System.out.println("[DEBUG] onDisable");
+	}
+}
